0feaebaa5f9a7b75a21d7b02ca8bf96b7a36a585
18513368704 <249105679@qq.com>
2020-07-02 11:05:47 +0800

    修改启动类位置,测试远程直接部署

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,11 +1,11 @@
-package com.yy.dubbo;
+package com.yy;
 
 
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-//咱也不知道,咱也不敢问,为啥提交不了,测试能不能打包
+//咱也不知道,咱也不敢问,为啥提交不了,测试能不能打包,再来启动
 @EnableDubbo
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class Application {
